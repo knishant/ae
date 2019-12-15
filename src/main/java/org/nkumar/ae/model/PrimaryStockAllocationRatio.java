4dcdb52520999596a080dca87f0cd9cd26f54dfd
Nishant Kumar <knishant@gmail.com>
2019-12-15 18:40:19 +0530

    moved interface to inner class

--- src/main/java/org/nkumar/ae/model/PrimaryStockAllocationRatio.java
+++ src/main/java/org/nkumar/ae/model/PrimaryStockAllocationRatio.java
@@ -45,4 +45,16 @@ public final class PrimaryStockAllocationRatio
             }
         });
     }
+
+    @FunctionalInterface
+    public interface Allocator
+    {
+        /**
+         * Try to allocate at most count items for the passed gender and shape.
+         * The count could be negative, in which case the gender and shape is already over allocated.
+         * Return the number that was actually allocate.
+         * @return count that could be allocated
+         */
+        int allocate(Gender gender, String shape, int count);
+    }
 }
